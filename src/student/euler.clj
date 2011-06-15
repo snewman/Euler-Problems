fd4125c3d96a3610462dfce44324603f8575ac4a
Sam Newman <sam.newman@gmail.com>
2011-06-15 09:18:05 +0100

    Stuff

--- src/student/euler.clj
+++ src/student/euler.clj
@@ -4,6 +4,34 @@
   [dividend divisor]
   (if (= (rem dividend divisor) 0) true false))
 
-(defn divides-any?
-  [divisor dividends]
-  (not (nil? (some #(divides? % 5) dividends))))
+(defn divides-any
+  "Return a predicate that tests whether its arg can be
+   evenly divided by any of nums."
+  [& nums]
+  (fn [arg]
+    (boolean (some #(divides? arg %) nums))))
+
+
+(defn euler-1
+  "Generates the list of values less than some upper bound which is divisible by 3 or 5"
+  ([]
+      (euler-1 1000))
+  ([upper]
+      (let [divisible? (divides-any 3 5)]
+        (loop [sum 0
+               n 1]
+          (if (>= n upper) sum
+              (recur (if (divisible? n) (+ sum n) sum) (inc n)))))))
+
+
+(defn euler-2
+  ([]
+      (euler-2 1000))
+  ([upper]
+      (apply + (filter (divides-any 3 5) (range upper)))))
+
+(defn euler-3
+  ([]
+      (euler-3 1000))
+  ([upper]
+     (->> (range upper) (filter (divides-any 3 5)) (apply +))))
